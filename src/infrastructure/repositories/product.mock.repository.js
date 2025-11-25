a5d2e318a92f4e6bbc26b4ffc4deb2afc41c809b
AdrianMontano528 <jadrian.montanot2011@gmail.com>
2025-11-25 19:17:19 -0400

    Fix the product mock repository

--- src/infrastructure/repositories/product.mock.repository.js
+++ src/infrastructure/repositories/product.mock.repository.js
@@ -1,14 +1,10 @@
-class MockProductRepository {
-  constructor() {
-    this.products = [];
-  }
-  async getAll() {
-    return this.products;
-  }
-  async create(productData) {
-    const newProduct = { ...productData, id: Date.now() };
-    this.products.push(newProduct);
-    return newProduct;
-  }
+class MockProductRepository { // ¡Implementa la interfaz!
+    constructor() { this.products = ['este es un producto','este es otro producto','tenemos mas productos']; }
+    async getAll() { return this.products; }
+    async create(productData) {
+        const newProduct = { ...productData, id: Date.now() };
+        this.products.push(newProduct);
+        return newProduct;
+    }
 }
 module.exports = MockProductRepository;
